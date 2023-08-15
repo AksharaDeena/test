5fea9cb7d4804a906cead0760b8ddba6f7e9b176
AksharaDeena <134607515+AksharaDeena@users.noreply.github.com>
2023-08-15 10:31:59 +0530

    add sleep after iteration

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import time
 
-def print_hello_world_and_sleep(iterations, sleep_duration):
+def print_hello_world_and_sleep(iterations):
     for i in range(1, iterations + 1):
         print(f"Hello World {i}")
-        time.sleep(sleep_duration)
+        
 
 def main():
     iterations = 5
     sleep_duration = 120  # 2 minutes in seconds
 
-    print_hello_world_and_sleep(iterations, sleep_duration)
+    print_hello_world_and_sleep(iterations)
+    time.sleep(sleep_duration)
 
 if __name__ == "__main__":
     main()
